69955c4411cf2adf642137a7b668f8549f38332a
SoalHuang <soso@SoalHuangMacAir.local>
2020-10-29 17:17:31 +0800

    upload

---
+++ Example/Pods/Target Support Files/Pods-SDFD_Tests/Pods-SDFD_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SDFD_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SDFD_Tests
+@end
---
+++ Example/Pods/Target Support Files/SDFD/SDFD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SDFD : NSObject
+@end
+@implementation PodsDummy_SDFD
+@end
